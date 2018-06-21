720139e565701cb3f0971f6b4743fcec9ec8b2b2
MSI\joy <joybuf526@gmail.com>
2018-06-21 12:09:18 -0400

    End Point Script is completely finished with UI and Reload Scene

--- Assets/Scripts/EndPoint.cs
+++ Assets/Scripts/EndPoint.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EndPoint : MonoBehaviour {
 
@@ -8,15 +10,23 @@ public class EndPoint : MonoBehaviour {
 	public GameObject Player;
 	public GameObject EndMusic;
 	public GameObject BGM;
+	public GameObject WinWords;
+	public GameObject Again;
+//	public Canvas canv;
 
 	// Use this for initialization
 	void Start () {
 		win = false;
+		WinWords.SetActive (false);
+		Again.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (win) {
+			WinWords.SetActive (true);
+			Again.SetActive (true);
+		}
 	}
 	public void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "Player") {
@@ -25,5 +35,9 @@ public class EndPoint : MonoBehaviour {
 			EndMusic.GetComponent<AudioSource> ().Play();
 			Player.GetComponent<thirdPersonCamera> ().StopPeanut ();
 		}
+	}
+
+	public void PlayAgain(){
+		SceneManager.LoadScene ("main");
 	}
 }
